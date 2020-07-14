c0995837c37edb8f5beaf05ee475023a4c472cbc
Mikhail Chelpanov <gimbarrworkout17@gmail.com>
2020-07-14 13:23:17 +0300

    Added      Matrix exponentiation.csproj Added      Matrix exponentiation.sln Added      Program.cs Added      bin/Debug/netcoreapp3.1/Matrix exponentiation.deps.json Added      bin/Debug/netcoreapp3.1/Matrix exponentiation.dll Added      bin/Debug/netcoreapp3.1/Matrix exponentiation.exe Added      bin/Debug/netcoreapp3.1/Matrix exponentiation.pdb Added      bin/Debug/netcoreapp3.1/Matrix exponentiation.runtimeconfig.dev.json Added      bin/Debug/netcoreapp3.1/Matrix exponentiation.runtimeconfig.json Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.AssemblyInfo.cs Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.AssemblyInfoInputs.cache Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.assets.cache Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.csproj.CoreCompileInputs.cache Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.csproj.FileListAbsolute.txt Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.csprojAssemblyReference.cache Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.dll Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.exe Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.genruntimeconfig.cache Added      obj/Debug/netcoreapp3.1/Matrix exponentiation.pdb Added      obj/Matrix exponentiation.csproj.nuget.dgspec.json Added      obj/Matrix exponentiation.csproj.nuget.g.props Added      obj/Matrix exponentiation.csproj.nuget.g.targets Added      obj/project.assets.json Added      obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace Matrix_exponentiation
+{
+    class Program
+    {
+        static bool flag = true;
+        static int cycles = 0;
+
+        static void Main(string[] args)
+        {
+            int n = 3; 
+            int power = 5;
+            int[,] matrix = new int[n,n];
+            int[,] keeper = new int[n,n];
+
+            matrix = initialization(matrix, n, false);
+            keeper = initialization(keeper, n, true);
+            Console.WriteLine("Our Initialized matrix:\n");
+            Display(matrix, n);
+
+            int[,] result = multiply(matrix, keeper, power, n);
+            Console.WriteLine("Result of exponentiation:\n");
+            Display(result, n);
+        }
+
+        static void Display(int[,] matrix, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n\n\n");
+        }
+
+        static int[,] multiply(int[,] matrix, int[,] keeper, int power, int n)
+        {
+            if (power <= 1 && cycles <= 0)
+                return matrix;
+            else if (power <= 1 && cycles >= 1)
+                return keeper;
+
+            if (flag)
+            {
+                flag = false;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            keeper[i, j] += matrix[i, k] * matrix[k, j];
+                        }
+                    }
+                }
+                return multiply(matrix, keeper, power - 1, n);
+            }
+
+            int[,] temp = new int[n, n];
+            temp = initialization(temp, n, true);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        temp[i, j] += keeper[i, k] * matrix[k, j];
+                    }
+                }
+            }
+            keeper = temp;
+            cycles++;
+            return multiply(matrix, keeper, power - 1, n);
+        }
+
+        static int[,] initialization(int[,] matrix,int n, bool iszero)
+        {
+            if (iszero)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            else
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = rnd.Next(0, 10);
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
